6b2582c97f62c7e3e228587957a1dd6179fdf8af
aubeidbarikzi <barikzi.aubeid@gmail.com>
2023-12-11 09:31:18 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Abschluss_Projekt_Android_Studio"
+include(":app")
